3996be5fedd128f0b5e54e8f274d1cbafd446ef1
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-09-18 10:39:06 +0300

    complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+const Keypad = () => {
+  const handleInput = (event) => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <input 
+      type="password"
+      onChange={handleInput}
+    />
+  );
+};
 
 export default Keypad;
